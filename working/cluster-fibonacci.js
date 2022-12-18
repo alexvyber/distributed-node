18b6a52d59223e4d3aeabc84bfdaab4e74698d2c
Alexey Sokolov <alexvyber@gmail.com>
2022-12-18 09:55:13 +0300

    finished 3 chapter

---
+++ working/cluster-fibonacci.js
@@ -0,0 +1,35 @@
+#!/usr/bin/env node
+
+// npm install fastify@3.2
+const server = require("fastify")()
+
+const HOST = process.env.HOST || "127.0.0.1"
+const PORT = process.env.PORT || 4000
+
+console.log(`worker pid=${process.pid}`)
+
+server.get("/:limit", async (req, reply) => {
+  return String(fibonacci(Number(req.params.limit)))
+})
+
+server.listen(PORT, HOST, () => {
+  console.log(`Producer running at http://${HOST}:${PORT}`)
+})
+
+function fibonacci(limit) {
+  let prev = 1n
+  let next = 0n
+  let swap
+
+  while (limit) {
+    swap = prev
+
+    prev = prev + next
+
+    next = swap
+
+    limit--
+  }
+
+  return next
+}
